9f7f0fe35614a102441d504073ced2ce2011068a
Benjamin H <hbenjamin@tutanota.de>
2022-04-18 20:55:18 +0200

    Erster Commit 👋

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="antialiased bg-slate-200 dark:bg-slate-900">
+    <main class="flex w-full h-screen justify-center items-center">
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+      <div class="w-full max-w-xl p-3">
+        <h1 class="font-bold text-5xl text-center text-red-500">
+          Jabakus 🧮
+        </h1>
+        <p class="font-bold text-1xl text-center text-red-400">Das coole Rechengame</p>
+        <div class="bg-slate-900 dark:bg-slate-700 p-12 rounded-lg shadow-lg w-full mt-8">
+          <h2 class="font-bold text-2xl text-center text-red-300">Einleitung</h2>
+          <p class="font-normal text-1xl text-center text-red-300">
+            Jabakus ist ein Highscorebasiertes Rechenspiel das 
+          </p>
+          <div class="flex items-center text-center p-6 space-x-2 rounded-b">
+            <button class="w-full text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Okay! Hab's verstanden!
+            </button>
+          </div>
+        </div>
+      </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        "hallo": "Yay"
+        }
+    }
+  }
+</script>
